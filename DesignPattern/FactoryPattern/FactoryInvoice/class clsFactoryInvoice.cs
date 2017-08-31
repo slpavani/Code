4c913cdcce17c6ffe7c9e21a9bb4e04a9e5061c4
Pavani Sikhakolli <sikhakolli_p@subway.com>
2017-08-30 18:56:47 -0700

    Factory Design pattern with Invoices

---
+++ DesignPattern/FactoryPattern/FactoryInvoice/class clsFactoryInvoice.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FactoryInvoice;
+using InvoiceInterface;
+using Invoice;
+
+namespace FactoryInvoice
+{
+    public class clsFactoryInvoice
+    {
+         public static IInvoice getInvoice(int intInvoicetype)
+         {
+            IInvoice objInvoice;
+            if (intInvoicetype == 1)
+            {
+                objInvoice = new clsInvoiceWithHeaders();
+            }
+            else if (intInvoicetype == 2)
+            {
+                objInvoice = new clsInvoiceWithOutHeaders();
+            }
+            else
+            {
+                return null;
+            }
+            return objInvoice;
+        }
+    }
+}
---
+++ DesignPattern/FactoryPattern/FactoryPattern/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using FactoryInvoice;
+using InvoiceInterface;
+
+
+namespace FactoryPattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int intInvoiceType = 0;
+            IInvoice objInvoice;
+            Console.WriteLine("enter invoice type");
+            intInvoiceType = Convert.ToInt16(Console.ReadLine());
+            objInvoice = clsFactoryInvoice.getInvoice(intInvoiceType);
+            objInvoice.print();
+            Console.ReadLine();
+        }
+    }
+}
